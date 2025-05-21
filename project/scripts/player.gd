ca4c734372fada4a8d15e488e268113490462417
Thomas Smart <thomassmart@Thomass-MacBook-Pro.local>
2025-05-21 23:24:26 +1000

    Add player states, run north with ball

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -2,34 +2,44 @@ extends CharacterBody2D
 
 signal ball_taken
 
-const SPEED = 50.0
+const RUN_SPEED: float = 50.0
 
-var target: Vector2
-var moving: bool = false
-var has_ball: bool = false
+enum State {
+	STANDING,
+	SEEKING_BALL,
+	HAS_BALL,
+	PASSING
+}
+
+var state: State = State.STANDING
+var move_target: Vector2
+
 
 func _physics_process(delta: float) -> void:
-	# Check if a target is assigned
-	if moving:
-		# Calculate the direction vector
-		var direction = target - self.position
-
-		# Normalize the direction vector
-		direction = direction.normalized()
-
-		# Set the velocity based on direction and speed
-		velocity = direction * SPEED
-
-		# Move the character body and handle collisions
-		move_and_slide()
+	if state == State.SEEKING_BALL:
+		_move_toward(move_target)
 
 		var collision: KinematicCollision2D = get_last_slide_collision()
 		if collision && collision.get_collider().name == "Ball":
-			moving = false
+			$Camera2D.make_current()
+			state = State.HAS_BALL
 			ball_taken.emit()
 			$Ball.visible = true
+	elif state == State.HAS_BALL:
+		_move_toward(Vector2(position.x, 0))
 
 
-func run_to_target(t):
-	moving = true
-	target = t
+func _move_toward(target: Vector2) :
+	# Calculate the direction vector
+	var direction = target - self.position
+	# Normalize the direction vector
+	direction = direction.normalized()
+	# Set the velocity based on direction and speed
+	velocity = direction * RUN_SPEED
+	# Move the character body and handle collisions
+	move_and_slide()
+
+
+func seek_ball(t):
+	state = State.SEEKING_BALL
+	move_target = t
--- project/scripts/team.gd
+++ project/scripts/team.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @export var ball: RigidBody2D
+@export var attack_direction: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Player.run_to_target(ball.position)
+	$Player.seek_ball(ball.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
